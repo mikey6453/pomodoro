770361176a50c996f01fa7d00a53a7cd8f06cfa3
Michael <tvq3bn@virginia.edu>
2024-03-07 17:14:23 -0500

    another commit

--- popup/popup.js
+++ popup/popup.js
@@ -1,106 +1,123 @@
 let tasks = []
 
-function updateTime() {
-    chrome.storage.local.get(["timer", "timeOption", "isRunning"], (res) => {
-        const time = document.getElementById("time");
-        // Determine if the timer has already finished.
-        const hasFinished = res.timer >= res.timeOption * 60;
-        
-        let minutes = "00";
-        let seconds = "00";
-
-        if (!hasFinished) {
-            minutes = `${res.timeOption - Math.ceil(res.timer / 60)}`.padStart(2, "0");
-            if (res.timer % 60 !== 0) {
-                seconds = `${60 - res.timer % 60}`.padStart(2, "0");
-            }
-        }
-
-        // Update the timer display accordingly.
-        time.textContent = `${minutes}:${seconds}`;
-
-        // If the timer has finished and is still marked as running, stop it.
-        if (hasFinished && res.isRunning) {
-            chrome.storage.local.set({ isRunning: false }, () => {
-                playAudio(); // Play the audio only once when the timer first finishes.
-            });
-        }
-
-        // Update the start/pause button text based on timer status.
-        startTimerBtn.textContent = res.isRunning ? "Pause Timer" : "Start Timer";
-    });
+// Plays a button click sound
+function playButton() {
+    const audio = new Audio(chrome.runtime.getURL('audio/button-sound.mp3'));
+    audio.play();
 }
 
-
-// PlayAudio function stays the same
-function playAudio() {
+// Plays a specific "Tuturu" sound
+function playTuturu() {
     const audio = new Audio(chrome.runtime.getURL('audio/tuturu.mp3'));
     audio.play();
 }
 
-
-
-function resetTimer() {
-    chrome.storage.local.set({
-        timer: 0,
-        isRunning: false,
-    }, () => {
+// Updates the timer display and controls button text based on timer state
+function updateTime() {
+    chrome.storage.local.get(["timer", "timeOption", "isRunning", "audioPlayed"], (res) => {
         const time = document.getElementById("time");
-        chrome.storage.local.get(["timeOption"], (res) => {
-            const minutes = `${res.timeOption}`.padStart(2, "0");
-            time.textContent = `${minutes}:00`;
-            startTimerBtn.textContent = "Start Timer";
-            updateTime();
-        });
+        const minutes = `${res.timeOption - Math.ceil(res.timer / 60)}`.padStart(2, "0");
+        let seconds = "00";
+        if (res.timer % 60 != 0) {
+            seconds = `${60 - res.timer % 60}`.padStart(2, "0");
+        }
+        time.textContent = `${minutes}:${seconds}`;
+
+        const startTimerBtn = document.getElementById("start-timer-btn");
+        startTimerBtn.textContent = res.isRunning ? "Pause Timer" : "Start Timer";
+
+        if (res.timer == 0 && !res.isRunning && !res.audioPlayed) {
+            playTuturu();
+            chrome.storage.local.set({audioPlayed: true}, () => {});
+        }
     });
 }
 
+// Call updateTime initially and then every second
 updateTime();
 setInterval(updateTime, 1000);
 
-const startTimerBtn = document.getElementById("start-timer-btn");
-startTimerBtn.addEventListener("click", () => {
-    chrome.storage.local.get(["isRunning"], (res) => {
-        chrome.storage.local.set({ isRunning: !res.isRunning }, () => {
-            startTimerBtn.textContent = !res.isRunning ? "Pause Timer" : "Start Timer";
+// Adds click event listeners to buttons with sound effect logic
+function addButtonListeners() {
+    const startTimerBtn = document.getElementById("start-timer-btn");
+    startTimerBtn.addEventListener("click", () => {
+        playButton(); // Play button sound on click
+        chrome.storage.local.get(["isRunning", "audioPlayed"], (res) => {
+            chrome.storage.local.set({
+                isRunning: !res.isRunning,
+                audioPlayed: false,
+            }, () => {
+                startTimerBtn.textContent = !res.isRunning ? "Pause Timer" : "Start Timer";
+            });
         });
     });
-});
 
-const resetTimerBtn = document.getElementById("reset-timer-btn");
-resetTimerBtn.addEventListener("click", () => {
-    chrome.storage.local.set({
-        timer: 0,
-        isRunning: false,
-    }, () => {
-        startTimerBtn.textContent = "Start Timer";
+    const resetTimerBtn = document.getElementById("reset-timer-btn");
+    resetTimerBtn.addEventListener("click", () => {
+        chrome.storage.local.set({
+            timer: 0,
+            isRunning: false,
+            audioPlayed: true // Ensure the end-of-timer sound does not play right after resetting
+        }, () => {
+            startTimerBtn.textContent = "Start Timer";
+        });
     });
-});
 
-const addTaskBtn = document.getElementById("add-task-btn");
-addTaskBtn.addEventListener("click", () => addTask());
+    const addTaskBtn = document.getElementById("add-task-btn");
+    addTaskBtn.addEventListener("click", () => {
+        playButton(); // Play button sound on click
+        addTask();
+    });
+}
 
+addButtonListeners(); // Initialize button listeners
+
+// Loads tasks from storage and renders them
 chrome.storage.sync.get(["tasks"], (res) => {
     tasks = res.tasks ? res.tasks : [];
     renderTasks();
 });
 
+// Adds a new task
+function addTask() {
+    tasks.push("");
+    saveTasks();
+    renderTasks();
+}
+
+// Deletes a task and updates the display
+function deleteTask(taskNum) {
+    tasks.splice(taskNum, 1);
+    saveTasks();
+    renderTasks();
+}
+
+// Saves tasks to storage
 function saveTasks() {
-    chrome.storage.sync.set({
-        tasks,
+    chrome.storage.sync.set({ tasks });
+}
+
+// Renders all tasks
+function renderTasks() {
+    const taskContainer = document.getElementById("task-container");
+    taskContainer.innerHTML = ''; // Clear existing tasks
+    tasks.forEach((task, index) => {
+        renderTask(task, index);
     });
 }
 
-function renderTask(taskNum) {
-    const taskRow = document.createElement("div");
+// Renders a single task
+function renderTask(task, index) {
+    const taskContainer = document.getElementById("task-container");
 
+    const taskRow = document.createElement("div");
     const text = document.createElement("input");
     text.type = "text";
     text.placeholder = "Enter a task...";
-    text.value = tasks[taskNum];
+    text.value = task;
     text.className = "task-input";
     text.addEventListener("change", () => {
-        tasks[taskNum] = text.value;
+        tasks[index] = text.value;
         saveTasks();
     });
 
@@ -109,33 +126,11 @@ function renderTask(taskNum) {
     deleteBtn.value = "X";
     deleteBtn.className = "task-delete";
     deleteBtn.addEventListener("click", () => {
-        deleteTask(taskNum);
+        playButton(); // Play button sound on click
+        deleteTask(index);
     });
 
     taskRow.appendChild(text);
     taskRow.appendChild(deleteBtn);
-
-    const taskContainer = document.getElementById("task-container");
     taskContainer.appendChild(taskRow);
-}
-
-function addTask() {
-    const taskNum = tasks.length;
-    tasks.push("");
-    renderTask(taskNum);
-    saveTasks();
-}
-
-function deleteTask(taskNum) {
-    tasks.splice(taskNum, 1);
-    renderTasks();
-    saveTasks();
-}
-
-function renderTasks() {
-    const taskContainer = document.getElementById("task-container");
-    taskContainer.textContent = "";
-    tasks.forEach((taskText, taskNum) => {
-        renderTask(taskNum);
-    });
 }
